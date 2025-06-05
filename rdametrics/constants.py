b102d89922e7beb8f6d24c471929b0a2c4e51dcc
alecramsay <4155251+alecramsay@users.noreply.github.com>
2025-06-05 05:39:18 -0700

    WIP

--- rdametrics/constants.py
+++ rdametrics/constants.py
@@ -5,7 +5,9 @@ CONSTANTS
 from typing import List, Dict
 
 states: List[str] = ["FL", "IL", "MI", "NC", "NY", "OH", "WI"]
+
 chambers: List[str] = ["congress", "upper", "lower"]
+
 ensemble_id_to_long_name: Dict[str, str] = {
     "A0": "S0.0_R0_Vcut-edges-rmst",
     "A1": "S0.0_R1_Vcut-edges-rmst",
@@ -25,13 +27,71 @@ ensemble_id_to_long_name: Dict[str, str] = {
     "R100": "S1.0_R0_Vcut-edges-region-aware",
 }
 ensembles: List[str] = list(ensemble_id_to_long_name.keys())
+
 categories: List[str] = [
     "general",
-    "partisan",
+    "proportionality",
+    "competitiveness",
     "minority",
     "compactness",
     "splitting",
     "mmd",
 ]
+
+metrics: Dict[str, List[str]] = {
+    "general": ["population_deviation"],
+    "proportionality": [
+        "estimated_vote_pct",
+        "estimated_seats",
+        "pr_deviation",
+        "disproportionality",
+        "fptp_seats",
+        "efficiency_gap_wasted_votes",
+        "efficiency_gap_statewide",
+        "efficiency_gap",
+        "seats_bias",
+        "votes_bias",
+        "geometric_seats_bias",
+        "declination",
+        "mean_median_statewide",
+        "mean_median_average_district",
+        "turnout_bias",
+        "lopsided_outcomes",
+        "proportionality",
+    ],
+    "competitiveness": [
+        "competitive_districts",
+        "competitive_district_count",
+        "average_margin",
+        "responsiveness",
+        "responsive_districts",
+        "overall_responsiveness",
+        "competitiveness",
+    ],
+    "minority": [
+        "mmd_black",
+        "mmd_hispanic",
+        "mmd_coalition",
+        "opportunity_districts",
+        "proportional_opportunities",
+        "coalition_districts",
+        "proportional_coalitions",
+        "minority",
+    ],
+    "compactness": [
+        "cut_score",
+        "reock",
+        "polsby_popper",
+        "population_compactness",
+        "compactness",
+    ],
+    "splitting": [
+        "counties_split",
+        "county_splits",
+        "county_splitting",
+        "district_splitting",
+        "splitting",
+    ],
+}
 
 ### END ###
